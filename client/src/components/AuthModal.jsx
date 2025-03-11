0499febb89679ad3bd308a783ef1861d6ab4a6ad
Sev4ik74k <sev4ikrnb@gmail.com>
2025-03-11 23:26:57 +0100

    test1

--- client/src/components/AuthModal.jsx
+++ client/src/components/AuthModal.jsx
@@ -38,7 +38,7 @@ const AuthModal = ({ open, onClose, authType }) => {
                     width: 400,
                     margin: "100px auto",
                     padding: 4,
-                    bgColor: "white",
+                    bgcolor: "white",
                     borderRadius: 2,
                     boxShadow: 3,
                 }}
