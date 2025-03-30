65f30d7e4c90b682ae5e93fb2c30a0191c4d3eea
Sev4ik74k <sev4ikrnb@gmail.com>
2025-03-30 18:18:42 +0200

    deleted old server

--- server/routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getAllReviews, addReview } = require("../controllers/reviewController");
-const router = express.Router();
-
-router.get("/", getAllReviews);
-router.post("/", addReview);
-
-module.exports = router;
